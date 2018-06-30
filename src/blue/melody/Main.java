8903a9fd56bc57621e9a86ee9fb6260a19aebd7f
Melody Kelly <melody@melody.blue>
2018-06-30 15:15:35 +1000

    Finished exercises.

--- src/blue/melody/Main.java
+++ src/blue/melody/Main.java
@@ -1,5 +1,7 @@
 package blue.melody;
 
+import java.util.*;
+
 public class Main {
 
     private static void easiestExerciseEver(String[] args) {
@@ -7,22 +9,22 @@ public class Main {
     }
 
     private static void drawHorizontalLine(String[] args) {
-        for (int n = 0; n < Integer.parseInt(args[2]); n++) {
+        for (int n = 0; n < Integer.parseInt(args[1]); n++) {
             System.out.print("*");
         }
         System.out.println();
     }
 
     private static void drawVerticalLine(String[] args) {
-        for (int n = 0; n < Integer.parseInt(args[2]); n++) {
+        for (int n = 0; n < Integer.parseInt(args[1]); n++) {
             System.out.println("*");
         }
     }
 
     private static void drawRightTriangle(String[] args) {
-        int size = Integer.parseInt(args[2]);
+        int size = Integer.parseInt(args[1]);
         for (int n = 0; n < size; n++) {
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i <= n; i++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -30,47 +32,139 @@ public class Main {
     }
 
     private static void drawIsoTriangle(String[] args) {
-        int size = Integer.parseInt(args[2]);
-        for (int n = 0; n < size; n++) {
-            for (int i = n; i < size; i++) {
+        int size = Integer.parseInt(args[1]);
+        for (int row = 1; row <= size; row++) {
+            for (int i = size - row; i > 0; i--) {
                 System.out.print(" ");
             }
-            for (int i = 0; i < size / 2; i++) {
-                System.out.print(" ");
+            for (int i = 0; i < ((2 * row) - 1); i++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 
+    private static void drawDiamond(String[] args) {
+        int size = Integer.parseInt(args[1]);
+        for (int row = 1; row <= size; row++) {
+            for (int i = size - row; i > 0; i--) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < ((2 * row) - 1); i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int row = size - 1; row > 0; row--) {
+            for (int i = size - row; i > 0; i--) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < ((2 * row) - 1); i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void drawDiamondName(String[] args) {
+        int size = Integer.parseInt(args[1]);
+        for (int row = 1; row < size; row++) {
+            for (int i = size - row; i > 0; i--) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < ((2 * row) - 1); i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("Melody");
+        for (int row = size - 1; row > 0; row--) {
+            for (int i = size - row; i > 0; i--) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < ((2 * row) - 1); i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        for (int i = 2; 2 * i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static Set<Integer> generate(int n) {
+        // TreeSet's are automatically sorted and unique
+        Set<Integer> factors = new TreeSet<>();
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                if (isPrime(i)) {
+                    factors.add(i);
+                }
+            }
+        }
+        return factors;
+    }
+
     public static void main(String[] args) {
-        switch (args[1]) {
+        if (args.length < 1) {
+            System.out.println("Use the first argument to select which exercise (1 - 9)");
+            return;
+        }
+
+        switch (args[0]) {
             case "1":
                 easiestExerciseEver(args);
                 break;
             case "2":
-                easiestExerciseEver(args);
+                drawHorizontalLine(args);
                 break;
             case "3":
-                easiestExerciseEver(args);
+                drawVerticalLine(args);
                 break;
             case "4":
-                easiestExerciseEver(args);
+                drawRightTriangle(args);
                 break;
             case "5":
-                easiestExerciseEver(args);
+                drawIsoTriangle(args);
                 break;
             case "6":
-                easiestExerciseEver(args);
+                drawDiamond(args);
                 break;
             case "7":
-                easiestExerciseEver(args);
+                drawDiamondName(args);
                 break;
             case "8":
-                easiestExerciseEver(args);
+                fizzBuzz();
                 break;
             case "9":
-                easiestExerciseEver(args);
+                for (int i : generate(Integer.parseInt(args[1]))) {
+                    System.out.print(i);
+                    System.out.print(", ");
+                }
+                System.out.println();
                 break;
+            default:
+                System.out.println("Please select a number between 1 and 9");
         }
     }
 }
